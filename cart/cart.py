6ab40d6e1b0ba244321a35d07e69112b7c5f0c61
ggyeong <s2018w31@e-mirim.hs.kr>
2020-09-18 15:39:51 +0900

    fix when add, +=quantity

--- cart/cart.py
+++ cart/cart.py
@@ -37,6 +37,7 @@ class Cart:
 
 
     def add(self, product, quantity=1, is_uqdate=False): #cart에 product 추가하기
+        product.id = str(product.id)
         if product.id not in self.cart:
             self.cart[product.id] = {'quantity': 0, 'price': str(product.price)}
 
